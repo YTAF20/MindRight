7c1f30edb0143642ddfef5e1f1f90ec96356f017
Vinny Huynh <151197482+vhuynh10@users.noreply.github.com>
2024-10-08 22:13:42 -0700

    Delete src directory

--- src/main/java/com/mindright/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.mindright.model;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- src/main/java/com/mindright/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mindright.repository;
-
-import com.mindright.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
